e2a410eb8721b85aa6f0ddd13a276985180f64cc
Fajar Setiawan <90747363+Fajar7701@users.noreply.github.com>
2021-10-01 00:35:12 +0700

    Add files via upload

---
+++ v_about.php
@@ -0,0 +1,24 @@
+<section>
+	<h1><?php echo $judul ?></h1>
+ 	<h4>Nama</h4>
+ 	<ul type="disc">
+ 		<li>Nama Depan : Fajar</li>
+ 		<li>Nama Belakang : Setiawan</li>
+ 		<li>Nim	: 19200864 </li>
+ 	</ul>
+ <br>
+ 	<h4>Alamat</h4>
+ 	<ul type="none">
+ 		<li> Jln. Bumi Persegi No.666</li>
+ 	</ul>
+ 
+ 	<h4>Tempat Lahir</h4>
+ 	<ul type="none">
+ 		<li>Bumi Sebelah Tenggara</li>
+ 	</ul>
+ 	<h4>Sebuah Teori Konspirasi</h4>
+ 	<ul type="square">
+ 		<li>Apakah Bumi itu Bulat ??</li>
+ 		<li>Atau Semesta ini yang Persegi ??</li>
+ 	</ul>
+</section>
---
+++ v_footer.php
@@ -0,0 +1,8 @@
+		<footer>
+			<center>
+			<a href="http://www.ApakahBumiBulat.com"> Sekian dari saya Terima Kasih </a>
+			</center>
+		</footer>
+	</div>
+</body>
+</html>
